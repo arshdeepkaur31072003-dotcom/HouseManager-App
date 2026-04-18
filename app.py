4bc23d0b3753e7b08519f48eebe4152aa44b44e6
Arshdeep Kaur <arshdeepakur31072003@gmail.com>
2026-04-18 17:03:19 +0530

    added gsheets link

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("🏠 Household Hub (Cloud Version)")
 # --- SIMPLE GOOGLE SHEETS SETTINGS ---
 # We will use a public CSV link from Google Sheets to keep it simple!
 SHEET_ID = "YOUR_SHEET_ID_HERE"
-url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv"
+url = f"https://docs.google.com/spreadsheets/d/{https://docs.google.com/spreadsheets/d/1O5N_R5ZGz-IQTV6YDt70ZtAHmWCbpisdtuYs8qkOlW8/edit?usp=sharing}/gviz/tq?tqx=out:csv"
 
 st.write("Welcome! Once we link your Google Sheet, your data will appear here.")
 st.info("Currently waiting for data connection...")
